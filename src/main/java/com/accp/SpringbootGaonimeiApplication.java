0c1ca8a1213f92fbe1b55aba7fa5253b18770e3d
zyy <1019632778.com>
2018-10-17 10:41:03 +0800

    第一次

---
+++ src/main/java/com/accp/SpringbootGaonimeiApplication.java
@@ -0,0 +1,12 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGaonimeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGaonimeiApplication.class, args);
+	}
+}
